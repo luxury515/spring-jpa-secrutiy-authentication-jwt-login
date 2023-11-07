59b774831f7b49e1ff7674449cf3337a1eee502b
채병춘 <luxury515@naver.com>
2023-11-07 14:57:43 +0900

    - jasypt 적용 , - application.yml 파일의 정보 암호화 / 복호화

---
+++ src/main/java/com/security/jwt/login/config/PropertyEncryptConfig.java
@@ -0,0 +1,40 @@
+package com.security.jwt.login.config;
+
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.testcontainers.shaded.org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.testcontainers.shaded.org.bouncycastle.pqc.jcajce.provider.BouncyCastlePQCProvider;
+
+@Configuration
+@EnableConfigurationProperties
+public class PropertyEncryptConfig {
+    @Bean("encryptBean")
+    public PooledPBEStringEncryptor stringEncryptor() {
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        encryptor.setProvider(new BouncyCastleProvider());
+        encryptor.setPoolSize(2);
+        encryptor.setPassword("Password");
+        encryptor.setAlgorithm("PBEWithMD5AndDES");
+        return encryptor;
+    }
+
+    public static void main(String[] args) {
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        encryptor.setProvider(new BouncyCastleProvider());
+        encryptor.setPoolSize(2);
+        encryptor.setPassword("Password");
+        encryptor.setAlgorithm("PBEWithMD5AndDES");
+
+        String username = "root";
+        String password = "root";
+        String encryptedUsername = encryptor.encrypt(username);
+        String encryptedPassword = encryptor.encrypt(password);
+        String decryptedUsername = encryptor.decrypt(encryptedUsername);
+        String decryptedPassword = encryptor.decrypt(encryptedUsername);
+        System.out.println("EncUsername:"+encryptedUsername+", DecUsername:"+decryptedUsername);
+        System.out.println("EncPassword:"+encryptedPassword+", DecPassword:"+decryptedPassword);
+
+    }
+}
