b6b90822f047c4ee9902107108057268e1e1b822
채병춘 <luxury515@naver.com>
2023-11-20 09:58:14 +0900

    -m

--- src/main/java/com/security/jwt/login/entity/User.java
+++ src/main/java/com/security/jwt/login/entity/User.java
@@ -14,11 +14,8 @@ import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 @Entity
 @Table(name = "users", uniqueConstraints = {
     @UniqueConstraint(columnNames = "username"),
@@ -33,7 +30,8 @@ public class User {
     private String username;
     private String email;
     private String password;
-    @ManyToMany(fetch = FetchType.EAGER)
+//    @ManyToMany(fetch = FetchType.EAGER)
+    @ManyToMany(fetch = FetchType.LAZY)
     @JoinTable(name = "user_roles",
         joinColumns = @JoinColumn(name = "user_id"),
         inverseJoinColumns = @JoinColumn(name = "role_id"))
